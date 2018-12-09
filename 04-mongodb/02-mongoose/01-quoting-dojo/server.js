b5104027eca80bae5a57150ecbe287502399b9ad
PatrickDewey <deweypt0651@student.laccd.edu>
2018-12-08 18:56:03 -0800

    initial commit

--- 04-mongodb/02-mongoose/01-quoting-dojo/server.js
+++ 04-mongodb/02-mongoose/01-quoting-dojo/server.js
@@ -13,9 +13,9 @@ app.use(bodyParser.urlencoded({extended:true}));
 // Require Path
 const path = require('path');
 // Set our Static Folder Directory
-app.use(express.static(path.join(__dirname, './static')));
+app.use(express.static(path.join(__dirname, './client/static')));
 // Set our Views Folder Directory
-app.set('views', path.join(__dirname, './views'));
+app.set('views', path.join(__dirname, './client/views'));
 // Set our view engine to ejs
 app.set('view engine', 'ejs');
 
@@ -25,47 +25,14 @@ app.use(session({
     resave: 'true',
     secret: 'secret'
 }));
-app.use(flash())
+app.use(flash());
 
 
 // connect mongoose
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/quoting_dojo', { useNewUrlParser: true });
 // Add user.js model
-const User = require('./models/user')
+const User = require('./models/user');
 
-app.get('/', (req,res) => {
-    res.render('index')
-})
-
-app.post('/user', (req,res) => {
-    const data = new User({name: req.body.name, quote: req.body.quote})
-    data.save(function(err) {
-        if(err){
-            console.log("We have an error!")
-
-            for(var key in err.errors){
-                req.flash('registration', err.errors[key].message);
-            }
-            res.redirect('/');
-        }
-        else {
-            console.log('Successfully created user!')
-            res.redirect('/')
-        }
-
-    })
-});
-
-app.get('/quotes', function(req,res,) {
-    const order = User.find({}).sort("-createdAt")
-    console.log(order)
-    User.find({}, function(err, users) {
-        try {
-            res.render('quotes', {user_data: users})
-        } catch(err) {
-            next(err);
-        }
-    })
-})
+require('./server/config/routes.js', (app));
 app.listen(8000, () => console.log(`Listening on Port 8000`))
